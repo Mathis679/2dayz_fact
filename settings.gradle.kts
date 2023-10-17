c40fdf0c25f111ce8ce08832f5b3d879aedcc190
Mathis Laurent <mathis.laurent.pro@gmail.com>
2023-10-17 15:48:03 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "2day'z Fact"
+include(":app")
