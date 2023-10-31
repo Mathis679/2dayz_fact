ea40b6d89e7a439f8db462da7aed28f08b752710
Mathis Laurent <mathis.laurent.pro@gmail.com>
2023-10-31 12:05:50 +0100

    minor clean

--- app/src/main/java/com/example/a2dayzfact/MainActivity.kt
+++ app/src/main/java/com/example/a2dayzfact/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     goToSource = {
                         openSource(it)
                     }
-                 )
+                )
             }
         }
     }
