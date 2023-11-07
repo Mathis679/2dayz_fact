13ca8c86817a360e4253c6e2e0de0ff84d16f9e0
Mathis Laurent <mathis.laurent.pro@gmail.com>
2023-11-07 17:07:18 +0100

    Add daily notification

--- app/src/main/java/com/example/a2dayzfact/AppApplication.kt
+++ app/src/main/java/com/example/a2dayzfact/AppApplication.kt
@@ -1,7 +1,19 @@
 package com.example.a2dayzfact
 
 import android.app.Application
+import com.example.a2dayzfact.core.AlarmScheduler
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class AppApplication : Application()
+class AppApplication : Application() {
+
+    @Inject
+    lateinit var alarmScheduler: AlarmScheduler
+
+    override fun onCreate() {
+        super.onCreate()
+        alarmScheduler.scheduleDailyAlarm()
+    }
+
+}
